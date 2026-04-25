747b42096b8fdf0e5d3cbc6d826c84a8c0e5e795
elinaliuhk <elinaliu.hk@gmail.com>
2026-04-25 22:28:11 +0800

    Add Supabase configuration file

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    url: 'https://syiwyvcmnwavimcbwoii.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5aXd5dmNtbndhdmltY2J3b2lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNTA0NDUsImV4cCI6MjA4ODYyNjQ0NX0.cAhu2pyfIphDVaD8WPBdPPahU9lyJqb29cjufVPg5S8'
+};
